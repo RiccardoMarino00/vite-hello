e626a548e1940b106054ec6f2af8c0201b640fbd
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-04-09 18:43:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script >
+import AppTitle from './components/AppTitle.vue'
+export default {
+    data(){
+        return {
+            message:'Ciao mondo'
+        }
+    },
+    mounted(){
+        console.log('mounted')
+    },   
+    components: {
+        AppTitle
+    },
+}
+</script>
+
+<template>
+    <div>{{ message }}</div>
+
+  <AppTitle />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data(){
+        return {
+            title:'La mia prima app con Vite'
+        }
+    },
+    mounted(){
+        console.log('mounted')
+    },
+}
+</script>
+
+<template>
+    <div class="contenitore">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<style>
+body{
+    font-family: sans-serif;
+    height: 100vh;
+    position: relative;
+}
+
+.contenitore{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
